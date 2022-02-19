92e0d9d75b13fe28fafaa4b02baf5466c0a6340c
Mia Reinshagen <miareinshagen@MacBook-Pro-de-Mia.local>
2022-02-20 00:07:13 +0100

    installation du controller user, partie Login + verification sur Insomnia

--- BACKEND/controllers/User.controllers.js
+++ BACKEND/controllers/User.controllers.js
@@ -21,3 +21,35 @@ exports.signup = (req, res, next) => {
       })
       .catch(error => res.status(500).json({ error }));
   };
+
+  // Permet de ce connecter a un compte existant
+  exports.login = (req, res, next) => {
+    Users.findOne({ where: {email: req.body.email } })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: 'Utilisateur introuvable !' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) {
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            }
+            res.status(200).json({
+              id: user.id,
+              firstname: user.firstname,
+              lastname: user.lastname,
+              username: user.username,
+              isAdmin: user.isAdminAccount,
+              token: jwt.sign(
+                { id: user.id, 
+                  firstname: user.firstname,
+                  isAdmin: user.isAdminAccount, },
+                process.env.TOKEN,
+                { expiresIn: '24h' }
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
--- BACKEND/routes/user.routes.js
+++ BACKEND/routes/user.routes.js
@@ -1,10 +1,13 @@
 // Route pour les utilisateur 
 const express = require('express');
 const router = express.Router();
+//const bouncer = require('express-bouncer')(10000, 60000, 5);
+
 
 const userCtrl = require('../controllers/User.controllers');
 
 // Route pour crée un compte, et route de connexion
 router.post('/signup',userCtrl.signup);
+router.post('/login',userCtrl.login);
 
 module.exports = router;
