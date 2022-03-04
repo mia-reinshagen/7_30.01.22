b10f1a9a79ac9ce9c18d5c5c52f05f23b535f0ed
Mia Reinshagen <miareinshagen@macbook-pro-de-mia.home>
2022-03-04 14:43:31 +0100

    creation de la partie NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,13 @@ import Home from './pages/Home.pages';
 import SignUp from './pages/Signup.pages';
 import SignIn from './pages/Signin.pages';
 import Profil from './pages/Profil.pages';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
 		<Router>
 			<GlobalStyle />
-			
+			<Navbar/>
 			<Switch>
 				<Route path="/home" exact component={Home} />
 				<Route path="/signup" exact component={SignUp} />
---
+++ frontend/src/data/NavbarData.js
@@ -0,0 +1,19 @@
+export const data = [
+	{
+		to: '/',
+		text: 'Home',
+		id: 'home',
+	},
+	{
+		to: '/signin',
+		text: 'Singin',
+	},
+	{
+		to: '/signup',
+		text: 'Signup',
+	},
+	{
+		to: '/profil',
+		text: 'Profil',
+	},
+];
